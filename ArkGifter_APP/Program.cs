74fc543258c63c0d8bd37806bd344112773dc084
Eric Carter <ericxcarter@gmail.com>
2023-06-20 19:19:15 -0500

    nuget, basic sql connection initialization
    Signed-off-by: Eric Carter <ericxcarter@gmail.com>

--- ArkGifter_APP/Program.cs
+++ ArkGifter_APP/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace app_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverName = @"INSPERIC23\SQLEXPRESS";
+            string databaseName = "Ark_Gifter"; 
+            string connectionString = $"data source={serverName}; database={databaseName}; Integrated Security=true;";
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+	    }
+       }
+    }
+}
