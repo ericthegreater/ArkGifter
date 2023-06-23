fd5d04a3c1751b58dc3b5f8666baf8438347a855
ericthegreater <ericxcarter@gmail.com>
2023-06-23 17:19:50 -0500

    initial web app (jack design from week 17)

---
+++ ArkGifter_WEBAPP/wwwroot/index.js
@@ -0,0 +1,13 @@
+function ArkGifter_WEBAPP() {
+
+    function handleClick() {
+        alert("Hello World");
+    };
+
+    var button1 = document.getElementById("button-01");
+
+    button1.addEventListener("click", handleClick);
+
+}
+
+ArkGifter_WEBAPP();
